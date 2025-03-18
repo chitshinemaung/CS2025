b555dba89e6174e599a7407ea90ec8dbcf5c1e20
CHIT SHINE MAUNG <chitshinemaung.mm@gmail.com>
2025-03-18 10:05:35 +0630

    18.3.2025

--- BMI Calculator/Program.cs
+++ BMI Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,18 +15,37 @@ namespace BMI_Calculator
             double height;
             double bmi;
 
+            Console.WriteLine("Enter your Weight(lb) and Hight (inches) :");
+            Console.WriteLine();
             Console.WriteLine("Enter your weight: ");
             weight = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Enter your height: ");
             height = Convert.ToDouble(Console.ReadLine());
 
-            bmi = weight * height;
+            bmi = (weight* 703) /  (height * height);  // BMI = weight * height
+            Console.WriteLine("Your BMI is: " + bmi);
 
-            if ( bmi <  )
-
+            if ( bmi < 18.5)
+            {
+                Console.WriteLine("You are underweight");
+            }
+            else
+            {
+                if ( bmi > 18.5 && bmi < 24.9)
+                {
+                    Console.WriteLine("You are normal weight");
+                }
+                else
+                {
+                    if ( bmi > 25 )
+                    {
+                        Console.WriteLine("You are overweight");
+                    }
+                    
+                }
+            
         }
-    }    
-        
-    
+    }
 }
+ 
