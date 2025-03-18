4546f06e11190dd351e1512572bb7f80250fe5cc
CHIT SHINE MAUNG <chitshinemaung.mm@gmail.com>
2025-03-18 13:31:03 +0630

    123

--- Range Calculator/Program.cs
+++ Range Calculator/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace Range_Calculator
 {
-    internal class Program
+    internal class RangeCalculator
     {
         static void Main(string [] args)
+        /*  1 - 50 ==> 50
+            51 - 75 ==> 25
+            76 - 150 ==> 75
+            151 - 200 ==> 50
+            201 >
+        */
         {
+            int r1, r2, r3, r4, r5, userinput;
+            userinput = 100;
+            //int userInput;
+            //Console.WriteLine("Enter any number: ");
+            //userInput = int.Parse(Console.ReadLine());
+
+            r1 = userinput > 0 && userinput <= 50 ? 50 : 0;
+            Console.WriteLine("This is the result R1: " + r1);
+            r2 = userinput > 50 && userinput <= 75 ? 25 : 0;
+            Console.WriteLine("This is the result R2: " + r2);
+            r3 = userinput > 75 && userinput <= 150 ? 75 : 0;
+            Console.WriteLine("This is the result R3: " + r3);
+            r4 = userinput > 150 && userinput <= 200 ? 50 : 0;
+            Console.WriteLine("This is the result R4: " + r4);
+            r5 = userinput > 200 ? 0 : 0;
+            Console.WriteLine("This is the result R5: " + r5);
+
         }
     }
 }
