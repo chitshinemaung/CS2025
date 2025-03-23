02882bc7b6ef66c8cb307db88fd278896bcf690c
CHIT SHINE MAUNG <chitshinemaung.mm@gmail.com>
2025-03-23 10:30:26 +0630

    123

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Day3
 {
@@ -10,32 +11,68 @@ namespace Day3
     {
         static void Main(string [] args)
         {
-            CSM: Console.Write("Enter Character: ");
-            char ch = Convert.ToChar(Console.ReadLine());
+            /* CSM: Console.Write("Enter Character: ");
+             char ch = Convert.ToChar(Console.ReadLine());
 
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
-            {
-                Console.WriteLine("This is Vowel");
-            }
-            else
-            {
-                Console.WriteLine("Not Vowel");
-            }
+             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+             {
+                 Console.WriteLine("This is Vowel");
+             }
+             else
+             {
+                 Console.WriteLine("Not Vowel");
+             }
 
-            switch (ch )
+             switch (ch )
+             {
+                 case 'a':
+                 case 'e':
+                 case 'i':
+                 case 'o':
+                 case 'u':
+                     Console.WriteLine("This is Vowel");
+                     break;
+                 default:
+                     Console.WriteLine("Not Vowel");
+                     break;
+             }
+             goto CSM; */
+            Console.WriteLine("Enter any object: ");
+            object obj;
+            obj = Console.ReadLine();
+
+            switch (obj)
             {
-                case 'a':
-                case 'e':
-                case 'i':
-                case 'o':
-                case 'u':
-                    Console.WriteLine("This is Vowel");
+                case int i when i > 55:
+                    Console.WriteLine("This is Integer");
+                    Console.WriteLine("i is greater than 55");
+                    break;
+                case int i when i < 55:
+                    Console.WriteLine("This is Integer");
+                    Console.WriteLine("i is less than 55");
+                    break;
+                case int i when i == 55:
+                    Console.WriteLine("This is Integer");
+                    Console.WriteLine("i is equal to 55");
+                    break;
+                case int i:
+                    Console.WriteLine("This is Integer");
+                    Console.WriteLine("i is unknown");
+                    break;
+
+                case string s: 
+                    Console.WriteLine("This is String");
+                    break;
+                case bool b:
+                    Console.WriteLine("This is Boolean");
+                    break;
+                case double d:
+                    Console.WriteLine("This is Double");
                     break;
                 default:
-                    Console.WriteLine("Not Vowel");
+                    Console.WriteLine("This is Unknown");
                     break;
             }
-            goto CSM;
 
         }
     }
