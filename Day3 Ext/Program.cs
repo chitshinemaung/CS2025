c218a47c037ecd4382141639cf5370c27ae039e3
CHIT SHINE MAUNG <chitshinemaung.mm@gmail.com>
2025-03-28 19:15:05 +0630

    123

---
+++ Day3 Ext/Program.cs
@@ -0,0 +1,11 @@
+﻿int cardNo = 12;
+
+    string cardName = cardNo switch
+    {
+        1 => "Ace",
+        11 => "Jack",
+        12 => "Queen",
+        13 => "King",
+        _ => cardNo.ToString()
+    };
+Console.WriteLine(cardName);
