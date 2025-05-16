624c902cc4a011521d2356a6a2d00d5dabf4a067
CHIT SHINE MAUNG <chitshinemaung.mm@gmail.com>
2025-05-16 13:51:01 +0630

    16.5.2024

--- Day 5/Program.cs
+++ Day 5/Program.cs
@@ -1,49 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//Day_5
+using System;
+using System.Collections;
+using System.Data.SqlTypes;
 using System.Text;
-using System.Threading.Tasks;
-
-namespace Day_5
+using System.Xml.Serialization;
+namespace Testing
 {
-    internal class Program
+    class Program
     {
-        static void Main(string [] args)
+        public static void Main(string [] agrs)
         {
-            int k = 5;
+            int k = 10;
             call(k);
-            Console.WriteLine("Original Value of K : " + k); //5
+            Console.WriteLine("Value of k : " + k);
 
-            //pass by value ref:
-            StringBuilder sb = new StringBuilder();
-            call2(sb);
-            Console.WriteLine(sb.ToString());
+            StringBuilder yy = new StringBuilder();
+            call2(yy);
+            Console.WriteLine("REF value " + yy.ToString());
 
-            int j = 9;
-            int j1 = 10;
-            call3(ref j, ref j1);
-            Console.WriteLine(j + j1);
+            int nn = 10;
+            call3(ref nn);
+            Console.WriteLine("Value of nn : " + nn);
+
+            int aaa = 10;
+            int bbb = 20;
+            call4(ref aaa, ref bbb);
+            Console.WriteLine(aaa + " ," + bbb);
+
+            int hh, hhh;
+            call5(out hh, out hhh);
+            Console.WriteLine("Value of Call from Main  : " + hh + "," + hhh);
+
+            //Local Method
+            Program PG = new Program();
+            PG.Dosquare();
+
+
 
         }
 
-        static void call3(ref int z, ref int z1)
-        {
-            z = z + 1;
-            z1 = z1 + 1;
-            Console.WriteLine(z + ", "+ z1);
-        }
-
-        static void call2 (StringBuilder s )
-        {
-            s.Append("ChitShine");
-        }
-
-        
-
-        static void call (int x)
+        //pass by value
+        static void call(int x)
         {
             x = x + 1;
-            Console.WriteLine(x); //6
+            Console.WriteLine("Method Value: " + x);
+        }
+
+        //pass by reference
+        static void call2(StringBuilder y)
+        {
+            y.Append("Hello");
+        }
+
+
+        //pass by reference
+        static void call3(ref int g)
+        {
+            g = g + 1;
+            Console.WriteLine("Value of ref Method : " + g);
+        }
+
+        //pass by reference
+        static void call4(ref int c, ref int d)
+        {
+            c = c + 1;
+            d = d + 1;
+            Console.WriteLine("Value of ref Method : " + c);
+            Console.WriteLine("Value of ref Method : " + d);
+        }
+
+        //pass by out
+        static void call5(out int hh, out int hhh)
+        {
+            hh = 100;
+            hhh = 200;
+            hh = hh + 10;
+            hhh = hhh + 11;
+            Console.WriteLine("Value of out Method : " + hh + "," + hhh);
+        }
+
+        //local Method
+
+        void Dosquare()
+        {
+            Console.WriteLine(100);
+            Console.WriteLine(200);
+
+            int square (int x)
+            {
+                return x = x * x;
+            }
         }
     }
+
 }
+
+
+
---
+++ Testing Area/Program.cs
@@ -0,0 +1,25 @@
+﻿//Testing Day_5
+using System;
+namespace Day5Testing
+{
+    class LocalMethod
+    {
+        static void Main(string [] args)
+        {
+            LocalMethod LM = new LocalMethod();
+            LM.Dosquare();
+
+        }
+
+        void Dosquare()
+        {
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+
+            int square (int x)
+            {
+                return x = x * x;
+            }
+        }
+    } 
+}
